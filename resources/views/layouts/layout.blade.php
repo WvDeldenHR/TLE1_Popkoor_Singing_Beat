d96341a15cf65e612bd8dbe2857c2dd0fd8e9460
WvDeldenHR <0960572@hr.nl>
2022-11-22 13:56:56 +0100

    forgot to delete commented code

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -58,40 +58,6 @@
                         </button>
                     @endguest
                 </div>
-
-
-                <!-- <div class="d-flex align-items-center">
-                        <div class="nav-user" aria-controls="nav-user" aria-expanded="false">
-                            <div class="nav-user-icon">
-                                <img src="img/icon/icon_user_001_212427_32x32.svg">
-                            </div>
-                            <div class="nav-user-content align-items-center">
-                                <span>{{ Auth::user()->name }}</span>
-                                <img class="nav-avatar"src="img/popkoor_singing_beat_001.jpg">
-                            </div>
-                            <div class="nav-dropdown">
-                                <div class="d-flex flex-column">
-                                    <a href="">Account</a>
-                                    <a href="">Repertoire</a>
-                                    <a href="">Leden</a>            
-                                    <a href="{{ route('logout') }}"
-                                            onclick="event.preventDefault();
-                                            document.getElementById('logout-form').submit();">
-                                            Uitloggen
-                                    </a>
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                            @csrf
-                                    </form>
-                                </div>
-                            </div>
-                        </div>
-                   
-                    <div class="nav-hamburger" aria-controls="nav-hamburger" aria-expanded="false">
-                        <div class="nav-hamburger-bar1"></div>
-                        <div class="nav-hamburger-bar2"></div>
-                        <div class="nav-hamburger-bar3"></div>
-                        <span class="visually-hidden">Menu</span>
-                    </div> -->
             </div>
         </header>
 
