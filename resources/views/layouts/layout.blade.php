ce898490e4bb9951d861f0dbfaf36452eafd92bd
WvDeldenHR <0960572@hr.nl>
2022-11-22 13:54:13 +0100

    navbar v0.2

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -4,6 +4,7 @@
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name = “theme-color” content = “#5f3f6d”>
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
         <title>Popkoor Singing Beat</title>
@@ -20,26 +21,46 @@
     </head>
 
     <body>
-        <header class="primary-header pt-4">
-            <div class="primary-header-content container d-flex align-items-center justify-content-between py-1">
-                <div class="d-flex align-items-center justify-content-between">
-                    <a href="{{ route('home')}}"><img src="" alt="Popkoor Singing Beat"></a>
-                    <nav class="nav">
-                        <ul class="nav-list" aria-label="Primary" role="list">
-                            <li><a href="">Over Ons</a></li>
-                            <li><a href="">Events</a></li>
-                            <li><a href="">Repertoire</a></li>
-                            <li><a href="">Fototalbum</a></li>
-                            <li><a href="">Contact</a></li>
-                        </ul>
-                    </nav>
+        <header class="primary-header">
+            <div class="primary-header-content">
+                <a class="nav-logo" href="{{ route('home')}}">
+                    <img src="img/popkoor_singing_beat_logo.svg" alt="Popkoor Singing Beat">
+                </a>
+                <div class="nav-hamburger-area" aria-controls="nav-hamburger">
+                    <div class="nav-hamburger" aria-expanded="false"></div>
                 </div>
-                <div class="d-flex align-items-center">
+                <nav class="nav">
+                    <div class="nav-content">
+                        <ul class="nav-list" aria-label="Primary" role="list">
+                            <li class="nav-list-item"><a class="nav-list-link" href="">Over Ons</a></li>
+                            <li class="nav-list-item"><a class="nav-list-link" href="">Events</a></li>
+                            <li class="nav-list-item"><a class="nav-list-link" href="">Repertoire</a></li>
+                            <li class="nav-list-item"><a class="nav-list-link" href="">Fotoalbum</a></li>
+                            <li class="nav-list-item"><a class="nav-list-link" href="">Contact</a></li>
+                        </ul>
+                    </div>
+                </nav>
+                <div class="nav-user">
                     @guest
                         @if (Route::has('login'))
-                        <a href="{{ route('login') }}" class="button | d-sm-none d-md-inline-flex">Inloggen</a>
+                        <a class="nav-btn" href="{{ route('login') }}">Inloggen</a>
+                        <a class="nav-user-icon-btn" href="{{ route('login') }}">
+                            <img class="nav-user-icon" src="img/icon/icon_user_001_212427_32x32.svg">
+                        </a>
                         @endif
                     @else
+                        <div class="nav-btn nav-user-btn" href="{{ route('login') }}">
+                            <span>{{ Auth::user()->name }}</span>
+                            <img class="nav-user-avatar"src="img/popkoor_singing_beat_001.jpg">
+                        </div>
+                        <button class="nav-user-icon-btn">
+                            <img class="nav-user-icon" src="img/icon/icon_user_001_212427_32x32.svg">
+                        </button>
+                    @endguest
+                </div>
+
+
+                <!-- <div class="d-flex align-items-center">
                         <div class="nav-user" aria-controls="nav-user" aria-expanded="false">
                             <div class="nav-user-icon">
                                 <img src="img/icon/icon_user_001_212427_32x32.svg">
@@ -52,7 +73,7 @@
                                 <div class="d-flex flex-column">
                                     <a href="">Account</a>
                                     <a href="">Repertoire</a>
-                                    <a href="">Leden</a>            <!--  Needs to be Admin Only -->
+                                    <a href="">Leden</a>            
                                     <a href="{{ route('logout') }}"
                                             onclick="event.preventDefault();
                                             document.getElementById('logout-form').submit();">
@@ -64,18 +85,21 @@
                                 </div>
                             </div>
                         </div>
-                    @endguest
+                   
                     <div class="nav-hamburger" aria-controls="nav-hamburger" aria-expanded="false">
                         <div class="nav-hamburger-bar1"></div>
                         <div class="nav-hamburger-bar2"></div>
                         <div class="nav-hamburger-bar3"></div>
                         <span class="visually-hidden">Menu</span>
-                    </div>
-                </div>
+                    </div> -->
             </div>
         </header>
 
         @yield('content')
+
+        <footer>
+            <div></div>
+        </footer>
         <script src="{{ asset('js/main.js')}}"></script>
     </body>
 </html>
