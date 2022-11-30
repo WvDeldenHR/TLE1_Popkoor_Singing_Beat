ccbb5a16fa69f7583005b2ead75ece767148a717
WvDeldenHR <0960572@hr.nl>
2022-11-30 15:07:40 +0100

    home code clean up and optimalization

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,17 +2,13 @@
 
 @section('content')
 <section class="py-7">
-        <div class="psb-hero container | d-grid align-items-center">
-            <div class="psb-hero-content-img">
-                <div class="psb-circle-medium-primary"></div>
-                <div class="psb-circle-small-primary"></div>
-                <div class="psb-circle-small-secondary-top"></div>
-                <div class="psb-circle-small-secondary-bottom"></div>
-                <img class="psb-hero-img" src="img/popkoor_singing_beat_001.jpg">
+        <div class="hm-content-1 even-column-2-r-auto container | d-grid align-items-center w-100">
+            <div class="m-auto">
+                <img class="hm-img" src="img/popkoor_singing_beat_001.jpg">
              </div>
-            <div class="psb-hero-content">
-                <h1 class="psb-hero-header">Zingen maakt blij</h1>
-                <p class="psb-hero-txt">
+            <div class="hm-header-content">
+                <h1 class="hm-header">Zingen maakt blij</h1>
+                <p class="hm-lg-txt">
                     zingen is vrijheid, zingen is gezellig,
                     <br></br>
                     zingen is samenwerking, zingen is los van alles,
@@ -20,8 +16,8 @@
                     zingen is gewoon leuk, dat vinden wij nou ook!
                 </p>
                 <div class="d-flex justify-content-end">
-                    <a href="" aria-label="facebook"><img class="psb-socials-icon | px-1" src="img/icon/icon_socials_facebook_001_5F3F6D_32x32.svg"></a>
-                    <a href="" aria-label="instagram"><img class="psb-socials-icon | px-1" src="img/icon/icon_socials_instagram_001_5F3F6D_32x32.svg"></a>
+                    <a href="" aria-label="facebook"><img class="hm-icon | px-1" src="img/icon/icon_socials_facebook_001_5F3F6D_32x32.svg"></a>
+                    <a href="" aria-label="instagram"><img class="hm-icon | px-1" src="img/icon/icon_socials_instagram_001_5F3F6D_32x32.svg"></a>
                 </div>
             </div>
         </div>
@@ -31,11 +27,11 @@
         <div class="d-flex justify-content-center pb-4">
             <h2 class="fs-800 fw-extra-bold">Events</h2>
         </div>
-        <div class="psb-events-boxes container | d-grid">
-            <div class="psb-events-column | px-5">
-                <h3 class="psb-events-header | mb-4 fs-700 fw-semi-bold">Lorum Ipsum</h3>
-                <p class="psb-events-sm-txt | mb-3 pt-3 fs-300 fw-light">8 November 2022</p>
-                <p class="psb-events-txt | my-4 pb-4 fs-500 fw-regular">
+        <div class="hm-content-2 even-column-3 container | d-grid gap-5">
+            <div class="content-box | py-5 px-5 w-100 mw-100">
+                <h3 class="py-4 fs-700 fw-semi-bold text-start">Lorum Ipsum</h3>
+                <p class="hm-sm-txt | pt-3 fs-300 fw-light">8 November 2022</p>
+                <p class="hm-txt | mb-4 py-4 fs-500 fw-regular">
                     Lorem ipsum dolor sit amet, 
                     consectetur adipiscing elit, 
                     sed do eiusmod tempor incididunt 
@@ -46,13 +42,13 @@
                     Ipsum nunc aliquet bibendum enim.
                 </p>
                 <div class="d-flex justify-content-end mb-4">
-                    <button class="psb-events-btn button-primary">Lees Verder</button>
+                    <button class="hm-btn button-primary">Lees Verder</button>
                 </div>
             </div>
-            <div class="psb-events-column | px-5">
-                <h3 class="psb-events-header | mb-4 fs-700 fw-semi-bold">Lorum Ipsum</h3>
-                <p class="psb-events-small-txt | mb-3 pt-3 fs-300 fw-light">8 November 2022</p>
-                <p class="psb-events-txt | my-4 pb-4 fs-500 fw-regular">
+            <div class="content-box | py-5 px-5 w-100 mw-100">
+                <h3 class="py-4 fs-700 fw-semi-bold text-start">Lorum Ipsum</h3>
+                <p class="hm-sm-txt | pt-3 fs-300 fw-light">8 November 2022</p>
+                <p class="hm-txt | mb-4 py-4 fs-500 fw-regular">
                     Lorem ipsum dolor sit amet, 
                     consectetur adipiscing elit, 
                     sed do eiusmod tempor incididunt 
@@ -63,13 +59,13 @@
                     Ipsum nunc aliquet bibendum enim.
                 </p>
                 <div class="d-flex justify-content-end mb-4">
-                    <button class="psb-events-btn button-primary">Lees Verder</button>
+                    <button class="hm-btn button-primary">Lees Verder</button>
                 </div>
             </div>
-            <div class="psb-events-column | px-5">
-                <h3 class="psb-events-header | mb-4 fs-700 fw-semi-bold">Lorum Ipsum</h3>
-                <p class="psb-events-small-txt | mb-3 pt-3 fs-300 fw-light">8 November 2022</p>
-                <p class="psb-events-txt | my-4 pb-4 fs-500 fw-regular">
+            <div class="content-box | py-5 px-5 w-100 mw-100">
+                <h3 class="py-4 fs-700 fw-semi-bold text-start">Lorum Ipsum</h3>
+                <p class="hm-sm-txt | pt-3 fs-300 fw-light">8 November 2022</p>
+                <p class="hm-txt | mb-4 py-4 fs-500 fw-regular">
                     Lorem ipsum dolor sit amet, 
                     consectetur adipiscing elit, 
                     sed do eiusmod tempor incididunt 
@@ -80,17 +76,17 @@
                     Ipsum nunc aliquet bibendum enim.
                 </p>
                 <div class="d-flex justify-content-end mb-4">
-                    <button class="psb-events-btn button-primary">Lees Verder</button>
+                    <button class="hm-btn button-primary">Lees Verder</button>
                 </div>
             </div>
         </div>
     </section>
 
     <section class="py-6">
-        <div class="psb-boxes-area container | d-grid">
-            <div class="psb-boxes | d-grid">
+        <div class="hm-content-3 even-column-2 container | d-grid">
+            <div class="hm-box content-box even-column-2-l-auto | d-grid">
                 <div>
-                    <span class="psb-circle"></span>
+                    <span class="hm-circle"></span>
                 </div>
                 <div>
                     <h3 class="mb-3 fs-500 fw-semi-bold">Popkoor Singing Beat</h3>
@@ -102,9 +98,9 @@
                     </p>
                 </div>
             </div>
-            <div class="psb-boxes | d-grid">
+            <div class="hm-box content-box even-column-2-l-auto | d-grid">
                 <div>
-                    <span class="psb-circle"></span>
+                    <span class="hm-circle"></span>
                 </div>
                 <div>
                     <h3 class="mb-3 fs-500 fw-semi-bold">Wie kan lid worden van Popkoor Singing Beat?</h3>
@@ -122,61 +118,61 @@
         <div class="d-flex justify-content-center pb-4">
             <h2 class="fs-800 fw-extra-bold">Even Voorstellen</h2>
         </div>
-        <div class="psb-carousel container | d-grid">
-            <div class="psb-carousel-column | d-flex flex-column align-items-center justify-content-center pt-5 pb-4 px-4">
+        <div class="psb-carousel hm-content-4 even-column-3 container | d-grid gap-5">
+            <div class="content-box | d-flex flex-column align-items-center justify-content-center pt-5 pb-4 px-4">
                 <h3 class="mb-3 fs-500 fw-semi-bold">Suzanne</h3>
-                <p class="mb-3 fs-500 fw-regular">
+                <p class="mb-3 fs-500 fw-regular text-center">
                     Mijn naam is Suzanne de Jonge en ik ben geboren op 
                     30 november 1987 te Rotterdam en vanaf augustus 2017 
                     mag ik mijzelf de songcoach van Singing Beat noemen, 
                     waar ik met enorm veel plezier mee aan de slag ga!
                 </p>
-                <button class="button-primary">Lees Verder</button>
+                <button class="hm-btn button-primary">Lees Verder</button>
             </div>
-            <div class="psb-carousel-column | d-flex flex-column align-items-center justify-content-center pt-5 pb-4 px-4">
+            <div class="content-box | d-flex flex-column align-items-center justify-content-center pt-5 pb-4 px-4">
                 <h3 class="mb-3 fs-500 fw-semi-bold">Diny</h3>
-                <p class="mb-3 fs-500 fw-regular">
+                <p class="mb-3 fs-500 fw-regular text-center">
                     Een groot aantal jaren geleden riep ik nog "Ik kan niet zingen". 
                     En nu zing ik al een groot aantal jaren bij het leukste koor
                     van Spijkenisse. Ik kan hier wel zingen, we zingen vanuit 
                     plezier en de liedjes zijn de liedjes die je hoort op de radio.
                 </p>
-                <button class="button-primary">Lees Verder</button>
+                <button class="hm-btn button-primary">Lees Verder</button>
             </div>
-            <div class="psb-carousel-column | d-flex flex-column align-items-center justify-content-center pt-5 pb-4 px-4">
+            <div class="content-box | d-flex flex-column align-items-center justify-content-center pt-5 pb-4 px-4">
                 <h3 class="mb-3 fs-500 fw-semi-bold">Nathalie</h3>
-                <p class="mb-3 fs-500 fw-regular">
+                <p class="mb-3 fs-500 fw-regular text-center">
                     Ik ben Nathalie Bos. Naast mijn functie als secretaris ben ik
                     de oprichtster van Singing Beat. Samen met Corina Versloot 
                     heb ik Singing Beat in 2001 opgericht. Inmiddels bestaat
                     het koor uit ca. 70 leden waar ik natuurlijk erg trots op ben. 
                     t is en blijft iedere week weer een feestje om te mogen zingen!
                 </p>
-                <button class="button-primary">Lees Verder</button>
+                <button class="hm-btn button-primary">Lees Verder</button>
             </div>
             <!-- Content for the carousel -->
             <!--
-            <div class="psb-carousel-column | d-flex flex-column align-items-center justify-content-center pt-5 pb-4 px-4">
+            <div class="content-box | d-flex flex-column align-items-center justify-content-center pt-5 pb-4 px-4">
                 <h3 class="mb-3 fs-500 fw-semi-bold">Jacques</h3>
-                <p class="mb-3 fs-500 fw-regular">
+                <p class="mb-3 fs-500 fw-regular text-center">
                     Sinds 2007 ben ik lid van het koor en had het eigenlijk veel 
                     eerder moeten doen. Wat is er leuker dan zingen en dan nog met 
                     elkaar. Dus als u dit leest en als u van zingen houdt, meldt 
                     u aan! Oh, ja, mijn naam is Jacques de Jonge en ben tevens 
                     penningmeester van ons koor.
                 </p>
-                <button class="button-primary">Lees Verder</button>
+                <button class="hm-btn button-primary">Lees Verder</button>
             </div>
-            <div class="psb-carousel-column | d-flex flex-column align-items-center justify-content-center pt-5 pb-4 px-4">
+            <div class="content-box | d-flex flex-column align-items-center justify-content-center pt-5 pb-4 px-4">
                 <h3 class="mb-3 fs-500 fw-semi-bold">Sheila</h3>
-                <p class="mb-3 fs-500 fw-regular">
+                <p class="mb-3 fs-500 fw-regular text-center">
                     Hallo Allemaal, Mijn naam is Sheila Schols. Ik ben mama van 
                     Yenna en Nova, vrouw van Patrick, dochter, schoondochter, 
                     vriendin en juf op een basisschool.
                 </p>
-                <button class="button-primary">Lees Verder</button>
+                <button class="hm-btn button-primary">Lees Verder</button>
             </div>
-            <div class="psb-carousel-column | d-flex flex-column align-items-center justify-content-center pt-5 pb-4 px-4">
+            <div class="content-box | d-flex flex-column align-items-center justify-content-center pt-5 pb-4 px-4">
                 <h3 class="mb-3 fs-500 fw-semi-bold">Gerda</h3>
                 <p class="mb-3 fs-500 fw-regular">
                     Hallo iedereen, mijn naam is Gerda en met veel plezier zing ik 
@@ -185,7 +181,7 @@
                     ben ik sinds kort algemeen bestuurslid en draag ik graag mijn 
                     steentje bij aan Singing Beat!
                 </p>
-                <button class="button-primary">Lees Verder</button>
+                <button class="hm-btn button-primary">Lees Verder</button>
             </div>
              -->
         </div>
