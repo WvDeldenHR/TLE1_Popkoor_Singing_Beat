81019f4d2f611c11a556f43aae7139827582b256
OzzyOs <Joshahursepuny@gmail.com>
2022-11-30 14:18:49 +0100

    carousel toegevoegd

--- public/js/main.js
+++ public/js/main.js
@@ -34,3 +34,19 @@ navUser.addEventListener("click", () => {
     : navUser.setAttribute("aria-expanded", true);
   dropdownContent.toggleAttribute("data-visible");
 });
+
+//Carousel 
+const slidesContainer = document.getElementById("slides-container");
+const slide = document.querySelector(".slide");
+const prevButton = document.getElementById("slide-arrow-prev");
+const nextButton = document.getElementById("slide-arrow-next");
+
+nextButton.addEventListener("click", () => {
+  const slideWidth = slide.clientWidth;
+  slidesContainer.scrollLeft += slideWidth;
+});
+
+prevButton.addEventListener("click", () => {
+  const slideWidth = slide.clientWidth;
+  slidesContainer.scrollLeft -= slideWidth;
+});
