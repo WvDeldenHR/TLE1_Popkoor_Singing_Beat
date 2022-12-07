1107a46cff8c439e8019484231c854cf0f4084fc
1028473 - Mees Muller <89028475+TheMazeIsAmazing@users.noreply.github.com>
2022-12-07 19:02:03 +0100

    Preparations Song.show, music player, JS. Also changes to Markable which is neccecary for the Front-End developers

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,6 +11,7 @@
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
+    <script src="{{ asset('js/songPlayHandler.js') }}" defer></script>
     <script src="{{ asset('js/songCreateHandler.js') }}" defer></script>
     <script src="{{ asset('js/fileExtentionGetter.js') }}" defer></script>
 
@@ -21,6 +22,7 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/songPlayer.css') }}" rel="stylesheet">
 
     <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
 </head>
