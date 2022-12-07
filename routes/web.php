58f17f5b6471610bb81893761f8d10451c73e79b
1028473 - Mees Muller <89028475+TheMazeIsAmazing@users.noreply.github.com>
2022-12-07 13:01:25 +0100

    Revert "small fixes"

--- routes/web.php
+++ routes/web.php
@@ -1,30 +1,18 @@
 <?php
 
-use App\Http\Controllers\EventController;
-use App\Http\Controllers\PlaylistController;
-use App\Http\Controllers\PhotoAlbumController;
-use App\Http\Controllers\SongController;
-use App\Http\Controllers\PhotoController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\HomeController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
 
 Auth::routes();
 
-Route::get('/', [HomeController::class, 'index'])->name('home');
-
-Route::resource('songs', SongController::class);
-Route::post('songs/{song:id}/favourite', [SongController::class, 'favourite'])->name('song.favourite');
-Route::get('favourites', [SongController::class, 'showFavourites'])->name('favourites');
-
-
-Route::resource('playlists', PlaylistController::class);
-
-Route::resource('photos', PhotoController::class);
-
-Route::resource('PhotoAlbums', PhotoAlbumController::class);
-
-Route::resource('events', EventController::class);
-
-
-
-
+Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
